c9143d9c026a59dc156159280267b3907b4ef80b
Chris Duke <cmduke73@hotmail.com>
2019-01-17 16:29:59 -0600

    Implemented full CRUD against user API.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var apiUsersRouter = require('./routes/api/users');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -26,6 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api/users', apiUsersRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
