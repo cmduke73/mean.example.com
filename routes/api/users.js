223332bdd84a445daeb3f4cb2ed654145d44f763
Chris Duke <cmduke73@hotmail.com>
2019-01-21 14:22:54 -0600

    added non-API endpoint for accessing user app

--- routes/api/users.js
+++ routes/api/users.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:userId', function(req,res){
-  
+
     var userId = req.params.userId;
      Users.findOne({'_id':userId}, function(err, user){
        if(err){
@@ -34,44 +34,44 @@ router.post('/', function(req, res) {
       first_name: req.body.first_name,
       last_name: req.body.last_name
     }), function(err, user){
-      
+
       if(err){
         return res.json({success: false, user: req.body, error: err});
       }
-  
+
       return res.json({success: true, user: user});
-      
+
     });
   });
 
   router.put('/', function(req, res){
 
     Users.findOne({'_id': req.body._id}, function(err, user){
-  
+
      if(err) {
        return res.json({success: false, error: err});
      }
-  
+
      if(user) {
-  
+
       let data = req.body;
-  
+
       if(data.username){
         user.username = data.username;
       };
-  
+
       if(data.email){
       user.email = data.email;
       };
-  
+
       if(data.first_name){
       user.first_name = data.first_name;
       };
-  
+
       if(data.last_name){
       user.last_name = data.last_name;
       };
-  
+
       user.save(function(err){
         if(err){
           return res.json({success: false, error: err});
@@ -79,25 +79,25 @@ router.post('/', function(req, res) {
           return res.json({success: true, user:user});
         }
       });
-  
+
      }
-  
+
     });
-    
+
   });
 
   router.delete('/:userId', function(req,res){
 
     var userId = req.params.userId;
-  
+
     Users.remove({'_id':userId}, function(err,removed){
-  
+
       if(err){
         return res.json({success: false, error: err});
       }
-  
+
       return res.json({success: true, status: removed});
-  
+
     });
-  
+
   });
