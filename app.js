6c8721839fb69ec608ab01ac4d44345e9f5b9a68
Chris Duke <cmduke73@hotmail.com>
2019-01-25 15:32:32 -0600

    Added configuration

--- app.js
+++ app.js
@@ -20,7 +20,11 @@ var usersRouter = require('./routes/users');
 var articlesRouter = require('./routes/articles');
 var app = express();
 
-var config = require('./config.dev');
+if(process.env.NODE_ENV==='production'){
+  var config = require('../config.prod');
+}else{
+  var config = require('./config.dev');
+}
 
 mongoose.connect(config.mongodb, { useNewUrlParser: true });
 
